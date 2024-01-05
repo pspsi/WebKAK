88a1555525b8f6898700432548231d6d5bd9e123
anoize <152575195+anoize@users.noreply.github.com>
2024-01-05 08:52:33 +0700

    EX2.php
    EX2.php

--- a.php
+++ a.php
@@ -8,7 +8,14 @@
 </head>
 <body>
     <?php
-    echo"hello world";
+    $a = 4;
+    $b = 10;
+    $c = ($b - 5) * $a;
+    echo"Result 1 =" . $c . "<BR>";
+    echo'Result 2 =' . $c . '<BR>';
+    echo"Result 3 = $c <BR>";
+    echo'Result 4 = $c <BR>';
+    echo"Result 5 =" . ($c + $b) . "<BR>";
     ?>
 </body>
 </html>
