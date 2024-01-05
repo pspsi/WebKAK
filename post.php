614a05c9fbed62e2fa58a2c6d564dfd4b6ec6de3
anoize <152575195+anoize@users.noreply.github.com>
2024-01-05 10:54:34 +0700

    Work4
    Work4

---
+++ post.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1 style="text-align:center ;">Webboard KakKak</h1>
+    <hr>
+    
+    <div style="text-align:center;" >ต้องการดูกระทู้หมายเลข<?php $id=$_GET ['id']; echo $id ."<BR> <BR>";?>
+</div>
+    <form >
+    <table style="border: 2px solid black; width: 25%" align="center">
+        <tr><td align="left" bgcolor="#6CD2FE">แสดงความคิดเห็น</td></tr>
+        <td align="center"><textarea name="message" id="" cols="60" rows="10" ></textarea></td>
+        <tr><td colspan="2" align="center"><input type="submit"value="ส่งข้อความ"></td></tr>
+    </table>
+    <br>
+    <center><a href="index.html">กลับไปหน้าหลัก</a></center>
+</form>
+</body>
+</html>
